549ec49b3b8e8bf11ed652ea24ddf14c4d9401ab
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-05-07 23:25:21 +0900

    some changes

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -2,43 +2,42 @@ const generateMarkdown =  (result)=>{
 
 const readMe=  
 `
-  
-  # ${result.projectTitle}
-  [![GitHub license](https://img.shields.io/badge/license-${result.license}-important.svg)](${result.gitHub})
+# ${result.projectTitle}
+[![GitHub license](https://img.shields.io/badge/license-${result.license}-important.svg)](${result.gitHub})
 
-    ## Description
-    ${result.projectDescription}
+## Description
+${result.projectDescription}
 
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    * 
-    ## Installation
-    To install the necessary dependencies, run the following command:
-    ${result.installSec}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* 
+## Installation
+To install the necessary dependencies, run the following command:
+${result.installSec}
 
-    ## Usage
-    ${result.usageSec}
+## Usage
+${result.usageSec}
 
-    ## License
-    This project uses the following license: [${result.license}].
-    ![License](https://img.shields.io/badge/License-${encodeURIComponent(result.license)}-blue.svg)
+## License
+This project uses the following license: [${result.license}].
+![License](https://img.shields.io/badge/License-${encodeURIComponent(result.license)}-blue.svg)
 
-    ## Contributing
-    ${result.contSec}
+## Contributing
+${result.contSec}
 
-    ## Tests
-    To run tests, run the following command:
-    ${result.testSec}
+## Tests
+To run tests, run the following command:
+${result.testSec}
 
-    ## Questions
-    If you have any questions about this repo, you can open an issue, or contact ${result.userName} directly at ${result.email}.
-    `
-    return readMe;
+## Questions
+If you have any questions about this repo, you can open an issue, or contact ${result.userName} directly at ${result.email}.
+`
+return readMe;
 
 }
 
